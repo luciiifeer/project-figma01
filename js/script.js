7af220f5ae775fb0d6474eaca727a0c63a99f102
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-01-24 23:18:34 +0600

    modified

--- js/script.js
+++ js/script.js
@@ -6,4 +6,16 @@ $('.main_banner_slidder').slick({
     autoplayspeed: 3000,
     speed: 1000
 });
+
+	
+$('.main_slider').slick({
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: `<i class="fa-solid fa-angle-left left"></i>`,
+    nextArrow: `<i class="fa-solid fa-angle-right right"></i>`,
+    centerMode: true,
+    centerPadding: '0px',
+});
+
 				
